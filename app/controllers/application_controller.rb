415c67bd2a35d61f05fed413bf7340613ae3042a
Kathleen Chung <kathleenchung6@gmail.com>
2017-02-25 13:45:15 -0500

    Say hello, bitchez

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hurro, world!"
+  end
+
+  def goodbye
+    render html: "peace"
+  end
 end
