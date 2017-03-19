33a88bbd08a30415ad5dc2860b8052978a0305e8
Kathleen Chung <kathleenchung6@gmail.com>
2017-03-19 15:32:51 -0400

    Add index to user emails, downcase emails before saving to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { self.email = email.downcase } # or "bang" method -- email.downcase!
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, 
